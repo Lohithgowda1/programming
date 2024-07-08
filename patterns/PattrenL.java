12fed0bbf37198a68604a2d421f9fd522aef05c7
Lohith Gowda <137599260+Lohithgowda1@users.noreply.github.com>
2024-07-08 15:21:02 +0530

    Add files via upload

---
+++ patterns/PattrenL.java
@@ -0,0 +1,15 @@
+package javainterviewprograms;
+
+public class PattrenL {
+	public static void main(String args[]) {
+		int n=5;
+		for(int i=1;i<n;i++) {
+			
+			System.out.println("*");
+		}
+		for(int j=1;j<=n;j++) {
+			System.out.print("* ");
+		}
+	}
+
+}
---
+++ patterns/Ptternbox.java
@@ -0,0 +1,19 @@
+package javainterviewprograms;
+
+public class Ptternbox {
+	public static void main(String args[]) {
+		int n=5;
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+			if(i==0||j==0||i==4||j==4) {
+				System.out.print("*");
+			}
+			else {
+				System.out.print(" ");
+			}
+		
+	}
+			System.out.println();
+	}
+}
+}
